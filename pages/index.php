e00ca44f7786aaa07f75cf9ad6a57e4735abdde2
Peter Stakoun <peter@stakoun.com>
2014-10-15 19:22:31 -0400

    Update index.php

--- pages/index.php
+++ pages/index.php
@@ -1,8 +1,19 @@
+<?php
+  session_start();
+  
+  if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
+    $username = $_SESSION['username'];
+    //$firstname = getfromdatabase
+  } else {
+    header('Location: http://www.site.com/login.php');
+  }
+?>
+
 <html>
   <head>
     <title>Social Network</title>
   </head>
   <body>
-    <?php echo "<p>Welcome, $firstname!</p>"; ?> 
+    <?php echo '<p>Welcome, $firstname!</p>'; ?>
   </body>
 </html>
