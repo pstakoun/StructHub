e7380504bb2b19cbb66a484cbc83faf5bcaa9b8a
Peter Stakoun <peter@stakoun.com>
2015-01-05 13:25:37 -0500

    Create contacts.php

---
+++ contacts.php
@@ -0,0 +1,28 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Social Network</title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	
+	<body>
+		<div id="titleBar">
+			<div id="titleBarWrap">
+				<div id="titleBarLogo">
+					<a href="index.php"><img src="images/logo.png"></a>
+				</div>
+			</div>
+		</div>
+		
+		<div id = "settings">
+			<!-- Settings form -->
+		</div>
+		
+		<div id = "sidebar">
+			<a href="index.php">Home</a>
+			<a href="#">Contacts</a>
+			<a href="messaging.php">Messaging</a>
+			<a href="settings.php">Settings</a>
+		</div>
+	</body>
+</html>
