6c9ec11ef7c661a3b94b5719f016d3bb7ea2357e
Peter Stakoun <peter@stakoun.com>
2015-02-16 21:41:38 -0500

    Begin work on messaging

--- public/messagefeed.php
+++ public/messagefeed.php
@@ -1,3 +1,20 @@
 <?php
 	echo("<h2>Messages</h2>");
+	// Get messages
+	$stmt = $conn->prepare("SELECT * FROM messages WHERE recipient = :id");
+	$stmt->bindParam(":id", $id);
+	$stmt->execute();
+	$messages = $stmt->fetchAll();
+	
+	echo("<div id=\"conversation\">");
+	if (!empty($messages)) {
+		foreach ($messages as $message) {
+			echo("<p id=\"user\">" . $message["firstname"] . " " . $message["lastname"] . "</p>");
+			echo("<p id=\"message\">" . $message["message"] . "</p>");
+		}
+	}
+	else {
+		echo("<h4>No messages.</h4>");
+	}
+	echo("</div>");
 ?>
--- public/messaging.php
+++ public/messaging.php
@@ -4,6 +4,15 @@
         header("Location: login.php");
         die();
     }
+	
+	$errorMessage = "";
+	// Connect to database
+	try {
+		$conn = new PDO("mysql:host=structhubdb.db.11405843.hostedresource.com;dbname=structhubdb", "structhubdb", "Cx!ak#Unm6Bknn54");
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	} catch(PDOException $e) {
+		$errorMessage = "<p id=\"error\">Could not connect to database.</p>";
+	}
 ?>
 
 <!DOCTYPE HTML>
@@ -25,8 +34,60 @@
 		</div>
 		
         <div id="content">
-            <div id = "messages">
-                <!-- Display messages -->
+            <div id = "conversations">
+                <?php
+					// Find contacts
+					$contacts = [];
+					$stmt = $conn->prepare("SELECT * FROM contacts WHERE user1 = :id AND status = 2");
+					$stmt->bindParam(":id", $id);
+					$stmt->execute();
+					$result = $stmt->fetchAll();
+					foreach ($result as $row) {
+						$contacts[] = $row["user2"];
+					}
+					$stmt = $conn->prepare("SELECT * FROM contacts WHERE user2 = :id AND status = 2");
+					$stmt->bindParam(":id", $id);
+					$stmt->execute();
+					$result = $stmt->fetchAll();
+					foreach ($result as $row) {
+						$contacts[] = $row["user1"];
+					}
+					
+					// Get active conversations
+					$conversations = [];
+					$stmt = $conn->prepare("SELECT * FROM messages WHERE sender = :id OR recipient = :id");
+					$stmt->bindParam(":id", $id);
+					$stmt->execute();
+					$result = $stmt->fetchAll();
+					foreach ($result as $row) {
+						$sender = $row["sender"];
+						$recipient = $row["recipient"];
+						if ($sender == $id && !in_array($recipient, $conversations)) {
+							$conversations[] = $recipient;
+						}
+						else if ($recipient == $id && !in_array($sender, $conversations)) {
+							$conversations[] = $sender;
+						}
+					}
+					
+					echo("<h2>Conversations</h2>");
+					if (!empty($conversations)) {
+						foreach ($conversations as $contact) {
+							$stmt = $conn->prepare("SELECT * FROM users WHERE id = :contact");
+							$stmt->bindParam(":contact", $contact);
+							$stmt->execute();
+							$result = $stmt->fetchAll();
+							$row = $result[0];
+							echo("<div id=\"conversation\">");
+							echo("<p id=\"user\">" . $row["firstname"] . " " . $row["lastname"] . "</p>");
+							// echo last message
+							echo("</div>");
+						}
+					}
+					else {
+						echo("<h4>No active conversations.</h4>");
+					}
+				?>
             </div>
         </div>
             
